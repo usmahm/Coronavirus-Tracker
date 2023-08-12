ac323a9370999ab0bf86c79662122bd812e1118c
Ahmad Usman <ahmadujuwon@gmail.com>
2023-08-12 10:47:22 +0100

    refactor: add scrollingtext header

--- refactor/src/component/Header/Header.tsx
+++ refactor/src/component/Header/Header.tsx
@@ -7,10 +7,19 @@ type Props = {
 
 const Header: React.FC<Props> = ({ lastUpdatedTime }) => {
   return (
-    <header className={styles.mainHeader}>
-        <h1>Coronavirus Tracker</h1>
-        {lastUpdatedTime && <p>Last updated<span>{lastUpdatedTime}</span></p>}
-    </header>
+    <>
+      <div className={styles.scrollContainer}>
+        <div>
+          <p className={styles.scrollText}>
+            Glad the COVID Era is Behind Us. This Website Remains as a Historic Reminder.
+          </p>
+        </div>
+      </div>
+      <header className={styles.mainHeader}>
+          <h1>Coronavirus Tracker</h1>
+          {lastUpdatedTime && <p>Last updated<span>{lastUpdatedTime}</span></p>}
+      </header>
+    </>
   )
 }
 
